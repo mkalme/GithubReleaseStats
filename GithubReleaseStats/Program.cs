673367e9e3d66066bb56b49f4f043bf33b5b6773
mkalme <mikelis.kalme@gmail.com>
2025-05-22 19:02:32 +0300

    Add project files.

---
+++ GithubReleaseStats/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Octokit;
+using System.Threading.Tasks;
+
+namespace GithubReleaseStats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "GoldenTadpole";
+            string repository = "Mapee";
+
+            var github = new GitHubClient(new ProductHeaderValue("GitHubReleaseLister"));
+            var releases = github.Repository.Release.GetAll(username, repository).Result;
+
+            Console.WriteLine($"List of releases for {username}/{repository}:");
+            foreach (var release in releases)
+            {
+                Console.WriteLine($"- Name: {release.Name}");
+                Console.WriteLine($"  Tag: {release.TagName}");
+                Console.WriteLine($"  Published at: {release.PublishedAt}");
+                Console.WriteLine($"  Assets:");
+                foreach (var asset in release.Assets)
+                {
+                    Console.WriteLine($"    - {asset.Name}: {asset.DownloadCount}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
